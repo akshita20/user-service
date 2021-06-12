6cc87694eeb9ad3cb30fd24790c231d2441af3dc
Akshita_Trehan <akshitatrehan95@gmail.com>
2021-06-13 00:35:56 +0530

    Add Eureka client + connect with book-service

--- src/main/java/com/practice/userservice/controller/SpringClientTestController.java
+++ src/main/java/com/practice/userservice/controller/SpringClientTestController.java
@@ -1,19 +1,35 @@
 package com.practice.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RefreshScope
 public class SpringClientTestController {
 
-    @Value("${msg}")
+    @Value("${msg:Hello from user-service}")
     private String msg;
+
+    private String bookUrl = "http://book-service/client-config";
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/client-config")
     public String getBooks() {
         return msg;
     }
+
+    @GetMapping("/connect-book-service")
+    public String getDataFromBookService(){
+        return restTemplate.getForObject(bookUrl,String.class);
+    }
+
+
 }
